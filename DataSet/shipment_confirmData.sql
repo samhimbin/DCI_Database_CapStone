a767e0012bea12f5d26a97730ca8dcf29c8dd3a8
samhimbin <131382618+samhimbin@users.noreply.github.com>
2023-06-05 11:13:39 -0400

    Adding Data set for schemas

---
+++ DataSet/shipment_confirmData.sql
@@ -0,0 +1,48 @@
+/*
+-- Query: SELECT * FROM ecommerce_capstone.shipment
+LIMIT 0, 1000
+
+-- Date: 2023-05-29 17:38
+*/
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('ATY30RPB3AB',10273,'2023-05-13','FEDEX','639-6480 Non, Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('CPM60OED1LB',10251,'2023-04-29','BLUEDART','679-2963 Semper. Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('CXP92UJX4YB',10248,'2023-05-02','USPS','P.O. Box 515, 4959 Tristique Road');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('DHQ68LJK5UL',10290,'2023-06-22','USPS','Ap #917-1439 Ut, Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('DIX86LPI7QR',10328,'2023-06-17','FEDEX','P.O. Box 182, 2995 Nunc Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('DNX81PQK9EK',10336,'2023-02-16','FEDEX','Ap #171-1746 Amet, Ave');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('DPR01LGC1NW',10345,'2022-12-01','FEDEX','Ap #751-7901 Malesuada Ave');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('DQB75TJW1NK',10339,'2023-04-12','FEDEX','Ap #344-8015 Ut, Avenue');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('EPP01WQB1GE',10325,'2022-12-22','FEDEX','Ap #223-7331 Ac Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('EUD21QUQ8ZU',10333,'2022-11-18','USPS','P.O. Box 374, 6996 Integer Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('FHE47HPQ0YP',10314,'2023-02-26','UPS','202-1509 Sit St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('FWN20TPG5OH',10297,'2023-03-08','USPS','9885 Cursus Avenue');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('GHY72XWU6QA',10305,'2023-05-18','USPS','Ap #971-9376 Adipiscing Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('GSI47THD5NO',10327,'2023-06-08','BLUEDART','678-8922 Fringilla Road');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('IPY00HQO8TB',10257,'2023-07-10','BLUEDART','669-2669 Consectetuer Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('JMU41LME4IH',10252,'2023-07-22','UPS','P.O. Box 517, 8211 A Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('JNP88YTY2DS',10329,'2023-07-05','FEDEX','P.O. Box 679, 8513 Cursus Ave');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('KHG21GUK4LL',10301,'2023-04-30','UPS','149-7774 Dictum. St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('KHO15KWQ5SE',10319,'2023-05-11','DHL','P.O. Box 294, 6029 Nulla St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('LJJ89ZCJ1KF',10317,'2023-04-23','USPS','P.O. Box 232, 5649 Rutrum Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('LQM23NKU1FK',10321,'2023-05-10','USPS','996-9913 Consequat, Ave');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('LUR03NUN3XS',10262,'2023-03-03','FEDEX','Ap #357-6416 Dolor St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('MSN67CCQ1ZU',10272,'2023-03-07','USPS','Ap #173-6239 Nec, Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('NER18NLJ7QI',10342,'2023-07-01','USPS','Ap #397-6709 Tincidunt Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('NZJ47LLN6YT',10335,'2023-03-30','USPS','523-9137 Malesuada Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('OHY40PRC7EN',10334,'2023-05-30','DHL','5274 Libero St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('OMV82YJA3FL',10286,'2023-01-19','BLUEDART','Ap #622-3221 Egestas Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('OVH52JCM5TP',10275,'2023-02-18','UPS','Ap #650-7265 Euismod St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('PBB34AQA1KT',10279,'2022-11-18','FEDEX','666-4418 Ac, Street');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('PLP44GBS8HC',10263,'2022-12-26','USPS','P.O. Box 711, 7732 Orci, Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('QGK60LUR0QQ',10281,'2023-07-14','FEDEX','280 Orci. Avenue');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('QRE70JKG2ZE',10254,'2022-12-30','UPS','Ap #275-4292 Phasellus Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('QSM70OBG4FY',10310,'2023-03-09','FEDEX','3066 Molestie St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('RRU58CAV6IY',10285,'2023-04-22','UPS','593-5174 Quisque Av.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('RYD26UHB6BE',10343,'2023-06-06','DHL','275-5875 Risus. Ave');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('SUF76YHK0TK',10332,'2023-02-25','DHL','438-8159 Duis Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('UKH35NAP8VM',10274,'2023-04-18','FEDEX','257-1592 Metus. Rd.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('VCS86UKP8MJ',10259,'2023-02-16','FEDEX','P.O. Box 830, 8868 Aliquet St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('VPW62EWW2AD',10306,'2023-02-23','USPS','Ap #958-2280 Pretium St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('YVS62MOQ5MM',10324,'2023-02-20','USPS','3082 Duis St.');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('YXH13WTY4LI',10260,'2023-05-29','UPS','Ap #950-1658 Consequat Avenue');
+INSERT INTO shipment (`shipment_id`,`order_id`,`ship_date`,`courier_name`,`ship_address`) VALUES ('ZNF49YCJ3PR',10340,'2022-11-16','USPS','893-1773 Sed, Road');
